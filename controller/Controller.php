27dd5f3efca7c5c5ad6f8ff6169a040926c14ec8
Bohemiaman <73557721+Bohemiaman@users.noreply.github.com>
2021-06-27 17:42:27 +0200

    add function to sanitize input

--- controller/Controller.php
+++ controller/Controller.php
@@ -10,8 +10,22 @@ abstract class Controller
     //postará se o poskytnutí view
     function displayView()
     {
+        $this->data = $this->sanitizeInput($this->data);
         extract($this->data);
         require("view/$this->view.phtml");
     }
-
+    function sanitizeInput($x = null)
+    {
+        if (!isset($x))
+            return null;
+        elseif (is_string($x))
+            return htmlspecialchars($x, ENT_QUOTES);
+        elseif (is_array($x)) {
+            foreach ($x as $k => $v) {
+                $x[$k] = $this->sanitizeInput($v);
+            }
+            return $x;
+        } else
+            return $x;
+    }
 }
