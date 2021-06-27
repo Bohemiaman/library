8a5365cf3e6e3f865addb96068057e22eeb19c8f
Bohemiaman <73557721+Bohemiaman@users.noreply.github.com>
2021-06-27 15:26:52 +0200

    Update BookHandler.php
    fixed variable name

--- model/BookHandler.php
+++ model/BookHandler.php
@@ -3,8 +3,7 @@ class BookHandler
 {
     function getAllBooks()
     {
-        $clanky = Db::retrieveAll("SELECT books.`name`, author.name, author.surname, author.nationality FROM `books` JOIN author ON books.author_id=author.author_id");
-        var_dump($clanky);
-        return $clanky;
+        $books = Db::retrieveAll("SELECT books.`name`, author.name, author.surname, author.nationality FROM `books` JOIN author ON books.author_id=author.author_id");
+        return $books;
     }
 }
