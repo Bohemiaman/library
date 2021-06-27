4e04bd577be892006e7fc42e9ff9890362988e30
Bohemiaman <73557721+Bohemiaman@users.noreply.github.com>
2021-06-27 17:47:00 +0200

    changed URL

--- model/BookHandler.php
+++ model/BookHandler.php
@@ -3,12 +3,13 @@ class BookHandler
 {
     function getAllBooks()
     {
-        $books = Db::retrieveAll("SELECT books.`name`, author.name, author.surname, author.nationality FROM `books` JOIN author ON books.author_id=author.author_id");
+        $books = Db::retrieveAll("SELECT books.`name`, books.`url`, author.name, author.surname, author.nationality FROM `books` JOIN author ON books.author_id=author.author_id");
         return $books;
     }
     function getSpecificBookByName($name)
     {
-        $book = Db::retrieveOne("SELECT books.`name`,books.`price`, books.`description`, author.name, author.surname, author.nationality FROM `books` JOIN author ON books.author_id=author.author_id WHERE books.`name` = :name", [':name' => $name]);
+        $book = Db::retrieveOne("SELECT books.`name`,books.`price`, books.`description`,book.`url`, author.name, author.surname, author.nationality FROM `books` JOIN author ON books.author_id=author.author_id WHERE books.`name` = :name", [':name' => $name]);
+
         return $book;
     }
 }
