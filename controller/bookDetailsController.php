04d565f2a02daaf505b889522498b6b2754216a3
Bohemiaman <73557721+Bohemiaman@users.noreply.github.com>
2021-06-27 17:58:57 +0200

    changed SQL queries to reflect changes in Database

--- controller/bookDetailsController.php
+++ controller/bookDetailsController.php
@@ -3,14 +3,14 @@ class BookDetailsController extends Controller
 {
     public function process($params = array())
     {
-        $bookName = $params[0];
+        $bookURL = $params[0];
         $bookHandler = new BookHandler();
-        $this->data["book"] = $bookHandler->getSpecificBookByName($bookName);
+        $this->data["book"] = $bookHandler->getSpecificBookByURL($bookURL);
 
         $this->header = array(
-            "title" => "Informace o knize $bookName[1]", "keyWords" => "knihy, knihovna, čtení", "description" => "Detaily knihy $bookName"
+            "title" => "Informace o knize" . $this->data["book"][0], "keyWords" => "knihy, knihovna, čtení", "description" => "Detaily knihy" . $this->data["book"][0]
         );
-        
+
         $this->view = 'bookDetail';
     }
 }
--- model/BookHandler.php
+++ model/BookHandler.php
@@ -3,12 +3,18 @@ class BookHandler
 {
     function getAllBooks()
     {
-        $books = Db::retrieveAll("SELECT books.`name`, books.`url`, author.name, author.surname, author.nationality FROM `books` JOIN author ON books.author_id=author.author_id");
+        $books = Db::retrieveAll("SELECT books.`name`, books.`url`, authors.name, authors.surname, authors.nationality FROM `books` JOIN authors ON books.author_id=authors.author_id");
         return $books;
     }
     function getSpecificBookByName($name)
     {
-        $book = Db::retrieveOne("SELECT books.`name`,books.`price`, books.`description`,book.`url`, author.name, author.surname, author.nationality FROM `books` JOIN author ON books.author_id=author.author_id WHERE books.`name` = :name", [':name' => $name]);
+        $book = Db::retrieveOne("SELECT books.`name`,books.`price`, books.`description`,books.`url`, authors.name, authors.surname, authors.nationality FROM `books` JOIN authors ON books.author_id=authors.author_id WHERE books.`name` = :name", [':name' => $name]);
+
+        return $book;
+    }
+    function getSpecificBookByURL($url)
+    {
+        $book = Db::retrieveOne("SELECT books.`name`,books.`price`, books.`description`,books.`url`, authors.name, authors.surname, authors.nationality FROM `books` JOIN authors ON books.author_id=authors.author_id WHERE books.`url` = :url", [':url' => $url]);
 
         return $book;
     }
