cc3b8022b2dae4b3656e088171a60e95ceeff420
Bohemiaman <73557721+Bohemiaman@users.noreply.github.com>
2021-06-28 16:41:33 +0200

    add user management system class with login and registration

---
+++ model/UserManagement.php
@@ -0,0 +1,31 @@
+<?php
+class UserManagement
+{
+    function selectUserFromDb($login)
+    {
+        $user = Db::retrieveOne("SELECT * FROM `users` WHERE `login`=:login", [':login' => $login]);
+        return $user;
+    }
+
+    function loginUser($login, $password): bool
+    {
+        $userInformation = $this->selectUserFromDb($login);
+        //ověření loginů je redundantní, ale pomůže to ošetřit chyby alespoň
+        $password_hash = password_hash($password, PASSWORD_BCRYPT);
+        return (($userInformation['login'] == $login) && ($userInformation['password_hash'] == $password_hash));
+    }
+    function registerUser($login, $password, $displayName, $admin = false): bool
+    {
+        //TODO REGEX na validaci do guard clause, nejspíše pomocí další metody
+        if (false) {
+            return false;
+        }
+
+        $password_hash = password_hash($password, PASSWORD_BCRYPT);
+        $executed = Db::executeQuery(
+            "INSERT INTO `users`(`login`, `displayName`, `password_hash`, `admin`) VALUES ( :login, :password_hash, :displayName, :admin)",
+            [':login' => $login, ':password_hash' => $password_hash, ':displayName' => $displayName, 'admin' => $admin]
+        );
+        return $executed;
+    }
+}
