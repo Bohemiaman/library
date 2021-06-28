d5c3e570646e5a312df65ca16cebcfbc40f17154
Bohemiaman <73557721+Bohemiaman@users.noreply.github.com>
2021-06-28 16:41:08 +0200

    add function to execute query and get boolean result

--- model/Db.php
+++ model/Db.php
@@ -13,16 +13,26 @@ class Db
         }
         return self::$connection;
     }
+
+    //vrátí vše
     public static function retrieveAll($query, $args = null)
     {
         $stmt = self::$connection->prepare($query);
         $stmt->execute($args);
         return $stmt->fetchAll();
     }
+    //vrátí 1 řádek
     public static function retrieveOne($query, $args = null)
     {
         $stmt = self::$connection->prepare($query);
         $stmt->execute($args);
         return $stmt->fetch();
+    }
+
+    //pro provedení příkazu, kde nás zajímá pouze, jesli proběhl
+    public static function executeQuery($query, $args = null)
+    {
+        $stmt = self::$connection->prepare($query);
+        return $stmt->execute($args);
     }
 }
