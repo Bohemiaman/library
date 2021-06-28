f6d6b0daaf6f4bfe1111eaa22e0af05f04bddd2a
Bohemiaman <73557721+Bohemiaman@users.noreply.github.com>
2021-06-28 17:35:03 +0200

    add login

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -7,5 +7,11 @@ class LoginController extends Controller
             "title" => "Přihlášení do systému", "keyWords" => "přihlášení, uživatel, login", "description" => "Přihlášení do systému"
         );
         $this->view = 'loginView';
+        //FIXME přímý přístup k postu
+        if (isset($_POST['login'])) {
+            $userManagement = new UserManagement();
+            //FIXME - ošetření
+            $userManagement->loginUser($_POST["login"],$_POST["username"])
+        }
     }
 }
