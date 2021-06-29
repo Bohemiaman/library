236483da8c948e20081419f98302ebfc8055ab8a
Bohemiaman <73557721+Bohemiaman@users.noreply.github.com>
2021-06-29 10:22:12 +0200

    add redirect if logged in on login page

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -7,9 +7,12 @@ class LoginController extends Controller
             "title" => "Přihlášení do systému", "keyWords" => "přihlášení, uživatel, login", "description" => "Přihlášení do systému"
         );
         $this->view = 'loginView';
+
+        $userManagement = new UserManagement();
+        if ($userManagement->checkIfUserIsLoggedIn())
+            $this->redirect("accountInformation");
         //FIXME přímý přístup k postu
         if (isset($_POST['login'])) {
-            $userManagement = new UserManagement();
             //FIXME - ošetření
             if ($userManagement->loginUser($_POST["username"], $_POST["password"])) {
                 $this->redirect("home");
