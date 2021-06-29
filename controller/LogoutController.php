7d5ea05d1a7d7ec8f8a8642281aa74fd1d3b9c76
Bohemiaman <73557721+Bohemiaman@users.noreply.github.com>
2021-06-29 11:12:34 +0200

    add logout keywords

--- controller/LogoutController.php
+++ controller/LogoutController.php
@@ -4,7 +4,7 @@ class logoutController extends Controller
     public function process($params = array())
     {
         $this->header = array(
-            "title" => "Přihlášení do systému", "keyWords" => "přihlášení, uživatel, login", "description" => "Přihlášení do systému"
+            "title" => "Odhlášení", "keyWords" => "odhlášení", "description" => "Odhlášení ze systému"
         );
         $this->view = 'logoutView';
         $userManagement = new UserManagement();
