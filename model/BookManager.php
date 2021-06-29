6df4de8560defcc4c54d71540f13628ea852b80b
Bohemiaman <73557721+Bohemiaman@users.noreply.github.com>
2021-06-29 12:21:03 +0200

    change to query in getAllBooks

--- model/BookManager.php
+++ model/BookManager.php
@@ -3,7 +3,7 @@ class BookManager
 {
     function getAllBooks()
     {
-        $books = Db::retrieveAll("SELECT books.`name`, books.`url`, authors.name, authors.surname, authors.nationality FROM `books` JOIN authors ON books.author_id=authors.author_id");
+        $books = Db::retrieveAll("SELECT books.`name`, books.`url`, authors.name, authors.surname, authors.nationality, books.price FROM `books` JOIN authors ON books.author_id=authors.author_id");
         return $books;
     }
     function getSpecificBookByName($name)
